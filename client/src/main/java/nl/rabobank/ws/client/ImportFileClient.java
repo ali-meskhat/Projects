3206e1988eacbeeb62a8038e7eb37bb171cfb0e8
meskham <ali.meskhat@rabobank.nl>
2016-07-01 13:51:39 +0200

    import-personBatch: Added new xsds for importing functionality

---
+++ client/src/main/java/nl/rabobank/ws/client/ImportFileClient.java
@@ -0,0 +1,9 @@
+package nl.rabobank.ws.client;
+
+/**
+ * Created by ame21103 on 1-7-2016.
+ */
+public class ImportFileClient {
+
+
+}
---
+++ service/src/main/java/nl/rabobank/mtom/ws/service/ImportFileService.java
@@ -0,0 +1,7 @@
+package nl.rabobank.mtom.ws.service;
+
+/**
+ * Created by ame21103 on 1-7-2016.
+ */
+public class ImportFileService {
+}
